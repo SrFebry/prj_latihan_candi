41722ea565df333b454dbdf6ee234cccc35d3159
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-10-28 18:42:23 +0700

    Latihan Candi
    Latihancandi mirip sama yang kemarin tapi yang ini tidak memiliki gambar galeri dibawah

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prj_latihan_candi/detail_candi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DetailCandiScreen(),
     );
   }
 }
